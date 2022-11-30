ca3ef615a4e8a60b59ddae2085a436c81ca4849d
Rodrigo Sotolani <rsotolani@gmail.com>
2022-11-30 12:12:18 -0300

    rotas atualizadas para CRUD no Banco de Dados MongoDB

--- routes/processo.routes.js
+++ routes/processo.routes.js
@@ -1,45 +1,47 @@
 import express from "express";
-import { uuid } from "uuidv4";
+import ProcModel from "../model/proc.model.js";
+//import { uuid } from "uuidv4";
 
 
 const roteador = express.Router()
 
-const processos = [
-    { 
-      _id: "e27ab2b1-cb91-4b18-ab90-5895cc9abd29", 
-      documentName: "Licitação Enap - Curso Web Dev",
-      status: "Em andamento", 
-      details: "Processo para capacitação de servidores públicos em desenvolvimento de aplicações na WEB. Parceria com Ironhack", 
-      dateInit: "28/11/2022", 
-      comments: ["Processo aberto", "Processo partiu para as partes assinarem", "Processo agora está em análise final", "Processo já tem data final"], 
-      dateEnd: "16/12/2022", 
-      setor: "enap" 
-    }, 
-    { 
-      _id: "ee5999d7-02e9-4b3d-a1ab-f067eef54173", 
-      documentName: "Licitação Compras - Notebooks", 
-      status: "Em andamento", 
-      details: "Processo de licitação para compra de notebooks", 
-      dateInit: "30/11/2022", 
-      comments: ["Processo em aberto e sem previsão de conclusão"], 
-      dateEnd: "", 
-      setor: "tre" 
-    }, 
-    { 
-      _id: "ec68dbb0-1880-4fb9-91d7-cdd0cb42d4eb", 
-      documentName: "Licitação Compras - Ar Condicionado", 
-      status: "Finalizado", 
-      details: "Processo de licitação para compra de ar-condicionado", 
-      dateInit: "15/11/2022", 
-      comments: ["Processo em aberto", "Processo finalizado"], 
-      dateEnd: "25/11/2022", 
-      setor: "trj" 
-    }
-  ]
+// const processos = [
+//     { 
+//       _id: "e27ab2b1-cb91-4b18-ab90-5895cc9abd29", 
+//       documentName: "Licitação Enap - Curso Web Dev",
+//       status: "Em andamento", 
+//       details: "Processo para capacitação de servidores públicos em desenvolvimento de aplicações na WEB. Parceria com Ironhack", 
+//       dateInit: "28/11/2022", 
+//       comments: ["Processo aberto", "Processo partiu para as partes assinarem", "Processo agora está em análise final", "Processo já tem data final"], 
+//       dateEnd: "16/12/2022", 
+//       setor: "enap" 
+//     }, 
+//     { 
+//       _id: "ee5999d7-02e9-4b3d-a1ab-f067eef54173", 
+//       documentName: "Licitação Compras - Notebooks", 
+//       status: "Em andamento", 
+//       details: "Processo de licitação para compra de notebooks", 
+//       dateInit: "30/11/2022", 
+//       comments: ["Processo em aberto e sem previsão de conclusão"], 
+//       dateEnd: "", 
+//       setor: "tre" 
+//     }, 
+//     { 
+//       _id: "ec68dbb0-1880-4fb9-91d7-cdd0cb42d4eb", 
+//       documentName: "Licitação Compras - Ar Condicionado", 
+//       status: "Finalizado", 
+//       details: "Processo de licitação para compra de ar-condicionado", 
+//       dateInit: "15/11/2022", 
+//       comments: ["Processo em aberto", "Processo finalizado"], 
+//       dateEnd: "25/11/2022", 
+//       setor: "trj" 
+//     }
+//   ]
 
 //criação das rotas
 
 //rota para home /
+
 roteador.get("/", (req, res) => {
     //req -> request -> requisição que vem do cliente
     //res -> response -> resposta para o cliente
@@ -51,70 +53,96 @@ roteador.get("/", (req, res) => {
   })
   
   //Acessar todos os processos
-  roteador.get("/all", (req, res) => {
-    console.log(processos);
-    return res.status(200).json(processos);
+  roteador.get("/all", async (req, res) => {
+    try {
+      const procs = await ProcModel
+        .find({},                   //find vazio -> todas as ocorrencias
+        { __v: 0, updatedAt: 0})  //projections -> define os campos que retornados
+        .sort({ age: 1 })         //sort() -> ordenação
+        .limit(100);              //limit() -> limite de quantos registros serao retornados
+
+      return res.status(200).json(procs)
+
+    }
+    catch (error){
+      console.log(error.errors);
+      return res.status(500).json(error.errors)
+    }
   });
   
   //POST - create
-  roteador.post("/create", (req, res) => {
-    console.log(req.body);
-  
-    const form = req.body
-  
-    processos.push({ ...form, _id: uuid()});
-  
-    return res.status(201).json(processos);
-  })
+  roteador.post("/create", async (req, res) => {
+    
+    try {
+      const form = req.body;
+
+      //criando um documento dentro da collection -> .create()
+      const newProc = await ProcModel.create(form);
+      
+      return res.status(201).json(newProc);
+
+    } catch (error) {
+      console.log(error.errors);
+      return res.status(500).json(error.errors);
+    }
+
+  });
   
   //DELETE - delete user
-  roteador.delete("/delete/:id", (req, res) => {
-    console.log("delete id=",req.params._id); //req.params -> { }por isso ele pode ser descontruido
-    const { id } = req.params;
-  
-    const deleteProc = processos.find((proc) => proc._id === id)
-  
-    if (!deleteProc) {
-      return res.status(400).json("Processo não encontrado");
+  roteador.delete("/delete/:id", async (req, res) => {
+
+    try {
+      const { id } = req.params;
+      const deleteProc = await ProcModel.findByIdAndDelete(id);
+
+      if (!deleteProc) return res.status(400).json({msg: "Processo não encontrado."});
+
+      const proc = await ProcModel.find();
+
+      return res.status(200).json(proc);
+
+    } catch (error) {
+      console.log(error.errors);
+      return res.status(500).json(error.errors);
     }
-  
-    const index = processos.indexOf(deleteProc);
-  
-    processos.splice(index, 1); //eliminar a partir do elemento do index, uma vez
-  
-    console.table(processos);
-    return res.status(200).json(processos);
   });
   
   //EDIT
-  roteador.put("/edit/:id", (req, res) => {
-    const { id } = req.params;
-  
-    //const form = req.body;
-  
-    const updateProc = processos.find((proc) => proc._id === id);
-    const index = processos.indexOf(updateProc);
-  
-    // if (form.documentName) updateProc.documentName = form.documentName;
-    // if (form.status) updateProc.status = form.status;
-    // if (form.details) updateProc.details = form.details;
-    // if (form.dateInit) updateProc.dateInit = form.dateInit;
-    // if (form.comments) updateProc.comments = form.comments;
-    // if (form.dateEnd) updateProc.dateEnd = form.dateEnd;
-    // if (form.setor) updateProc.setor = form.setor;
-  
-    processos[index] = {
-      ...updateProc,
-      ...req.body,
+  roteador.put("/edit/:id", async (req, res) => {
+
+    try {
+      const { id } = req.params;
+      const updatedProc = await ProcModel.findByIdAndUpdate(
+        id,
+        { ...req.body},
+        { new: true, runValidators: true }
+      );
+
+      return res.status(200).json(updatedProc)
+
+    } catch (error) {
+      console.log(error.errors);
+      return res.status(500).json(error.errors);
     }
-  
-    return res.status(200).json(processos);
   });
   
   
   //Acessar um processo pelo ID GET /process/:id
-  roteador.get("/process/:id", (req, res) => {
-    const { id } = req.params;
+  roteador.get("/process/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const proc = await ProcModel.findById(id);
+
+      if (!proc) return res.status(400).json({msg: "Processo não encontrado."});
+
+      return res.status(200).json(proc);
+
+    } catch (error) {
+      console.log(error.errors);
+      return res.status(500).json( {msg: "Processo não encontrado"});
+    }
+
+    
   
     const processId = processos.find((proc) => proc._id === id);
     console.log(processId);
